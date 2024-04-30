c13892f4a27dc015dba27acbaa2c9721b9a63138
Jessica Maria <jessica.lopes93@hotmail.com>
2024-04-30 19:13:07 -0300

    concatenação

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.print(nomeCompleto);
+        System.out.println("\n");
+
+        String concatenacao ="?"; 
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+        
+    }
+}
