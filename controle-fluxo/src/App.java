e86c645f64197aca4a04466eec9d9d9519fa6608
Jessica Maria <jessica.lopes93@hotmail.com>
2024-05-09 18:53:45 -0300

    Try/catch

--- controle-fluxo/src/App.java
+++ controle-fluxo/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean condicao = false;
+
+
+
+        while(condicao){
+
+            System.out.println("executou ... ");
+
+        }
+
+
+
+
+
+
     }
 }
